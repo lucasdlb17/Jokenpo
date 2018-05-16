b6025e2d865a089dd7b83e307b7173bd0d6f5fcf
lucasdlb17 <38433219+lucasdlb17@users.noreply.github.com>
2018-05-16 11:46:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+	var vencedor;
+	var pontosDoJogador = 0;
+	var pontosDoComputador = 0;
+
+function Jogo(escolhaDoJogador) {
+	var escolhaDoJogador;
+	var escolhaDoComputador = Math.floor(Math.random()* 3);
+	switch(escolhaDoComputador){
+		case 0:
+			escolhaDoComputador = '<img src=pedra_computador.png class=escolha-mao>';
+		break;
+		case 1:
+			escolhaDoComputador = '<img src=papel_computador.png class=escolha-mao>';
+		break;
+		case 2:
+			escolhaDoComputador = '<img src=tesoura_computador.png class=escolha-mao>';
+		break;
+	}
+	if (escolhaDoJogador === '<img src=pedra_jogador.png class=escolha-mao>') {
+		if (escolhaDoComputador === '<img src=tesoura_computador.png class=escolha-mao>') {
+			vencedor = 'Jogador';
+		}
+		else if (escolhaDoComputador === '<img src=papel_computador.png class=escolha-mao>') {
+			vencedor = 'Computador';
+		} 
+		else if (escolhaDoComputador === '<img src=pedra_computador.png class=escolha-mao>') {
+			vencedor = 'Empatou';
+		}
+	} 
+	else if (escolhaDoJogador === '<img src=papel_jogador.png class=escolha-mao>') {
+		if (escolhaDoComputador === '<img src=pedra_computador.png class=escolha-mao>') {
+			vencedor = 'Jogador';
+		} 
+		else if (escolhaDoComputador === '<img src=tesoura_computador.png class=escolha-mao>') {
+			vencedor = 'Computador';
+		} 
+		else if (escolhaDoComputador === '<img src=papel_computador.png class=escolha-mao>') {
+			vencedor = 'Empatou';
+		}
+	} 
+	else if (escolhaDoJogador === '<img src=tesoura_jogador.png class=escolha-mao>') {
+		if (escolhaDoComputador === '<img src=papel_computador.png class=escolha-mao>') {
+			vencedor = 'Jogador';
+		} 
+		else if (escolhaDoComputador === '<img src=pedra_computador.png class=escolha-mao>') {
+			vencedor = 'Computador';
+		} 
+		else if (escolhaDoComputador === '<img src=tesoura_computador.png class=escolha-mao>') {
+			vencedor = 'Empatou';
+		}
+	}
+	
+	if (vencedor === 'Computador'){
+		pontosDoComputador = pontosDoComputador + 1;
+	}else if(vencedor === 'Jogador') {
+		pontosDoJogador = pontosDoJogador + 1;
+	}
+	
+	document.getElementById("escolha-do-jogador").innerHTML = escolhaDoJogador;
+	document.getElementById("placar-jogador").innerHTML = pontosDoJogador;
+	document.getElementById("escolha-do-computador").innerHTML = escolhaDoComputador;
+	document.getElementById("placar-computador").innerHTML = pontosDoComputador;
+	document.getElementById("resultado").innerHTML = vencedor;
+}
